bc13f3225ce6c6d0b5ea4f7a95f7bdca91a99aa6
Uxiono33 <alejmaru@myuax.com>
2024-10-21 19:33:29 +0200

    Update Celsius to Farenheit.py

--- Celsius to Farenheit.py
+++ Celsius to Farenheit.py
@@ -1,12 +1,6 @@
-Cº=float(input("introduce temperature in Cº"))
-def Cº_Fº(Cº):
-    return ((Cº*9/5)+32) 
+Cº = float(input("introduce temperature in Cº"))
+def Cº_Fº(a):
+    return ((a*9/5)+32) 
 
-velocity = float(input("enter the velocity in m/s: "))
-    KE = (1/2 * mass * velocity**2)
-    print (KE)
 
-mass = float(input("Are required positive values of mass, enter the positive mass in kg: "))
-    velocity = float(input("enter the velocity in m/s: "))
-    KE = (1/2 * mass * velocity**2)
-    print (KE)
+print(Cº_Fº(Cº))
